0b60105ecb7d1e75128eadf56c7735f5357d9564
juwenzhang <3137536916@qq.com>
2025-07-05 04:42:40 +0800

    fix: fix eslint lint * 2

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -28,6 +28,10 @@ const eslintConfig = [
       "@typescript-eslint/no-empty-object-type": "off",
       "@typescript-eslint/no-unused-vars": "off",
       "@typescript-eslint/no-require-imports": "off",
+      "@typescript-eslint/no-unused-expressions": "off",
+      "@typescript-eslint/no-unnecessary-type-constraint": "off",
+      "@typescript-eslint/no-this-alias": "off",
+      "@typescript-eslint/no-unsafe-function-type": "off",
     },
   }
 ];
