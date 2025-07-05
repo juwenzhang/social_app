9797df040094296c34fde859005c74dac01ed5d1
juwenzhang <3137536916@qq.com>
2025-07-05 13:24:58 +0800

    fix: update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -33,7 +33,7 @@ const eslintConfig = [
       "@typescript-eslint/no-unnecessary-type-constraint": "off",
       "@typescript-eslint/no-this-alias": "off",
       "@typescript-eslint/no-unsafe-function-type": "off",
-      "@typescript-eslint/no-wrapper-object-type": "off",
+      "@typescript-eslint/no-wrapper-object-types": "off",
       "react-hooks/exhaustive-deps": "off",
     },
   }
